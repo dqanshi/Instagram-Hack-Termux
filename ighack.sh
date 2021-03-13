3a55f48257958b83b372f592b2e1da5a112ad1ef
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-03-13 10:03:37 +0530

    Update ighack.sh

--- ighack.sh
+++ ighack.sh
@@ -69,7 +69,7 @@ echo '
 printf "\n"
 printf "                \e[101m\e[1;77m  >>  Script By N17R0 << \e[0m\n"
 printf "\n"
-printf "       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Noob Hackers \e[100m\e[1;77m << \e[0m\n"
+printf "       \e[100m\e[1;77m >>  Telegram : \e[1;96m @PythonProgramingIN \e[100m\e[1;77m << \e[0m\n"
 printf "\n"
 echo 
 
